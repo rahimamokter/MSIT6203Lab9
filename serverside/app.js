da3ded7d4f65d31de2a819da77ea7ddfede796f1
Rahima Mokter <rahimamokter@gmail.com>
2020-10-17 16:07:05 -0400

    Lab 9 all parts works

--- serverside/app.js
+++ serverside/app.js
@@ -1,26 +1,6 @@
 const express = require('express');
 const bodyParser  = require('body-parser');
 const app = express();
-
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
-
-
-// serve incoming post requests to /students
-app.post('/students', (req, res, next) => {
-  const student = req.body;
-  console.log(student.firstName + " " + student.lastName);
-  //sent an acknowledgment back to caller 
-  res.status(201).json('Post successful');
-});
-
-
-
-
 // use the following code on any request that matches the specified mount path
 app.use((req, res, next) => {
    console.log('This line is always called');
@@ -29,16 +9,29 @@ app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
    next();
 });
-app.get('/students', (req, res, next) => {
-  const students = [ 
-    { "id" : "1", "firstName" : "John" , "lastName" : "Dow" }, 
-    { "id" : "2", "firstName" : "Ann" , "lastName" : "Smith" }, 
-    { "id" : "3", "firstName" : "Joan" , "lastName" : "Doe" }];
-//send the array as the response 
-   res.json(students);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
-});
-
-
-//to use this middleware in other parts of the application
-module.exports=app;
+// serve incoming post requests to /students
+app.post('/students', (req, res, next) => {
+    const student = req.body;
+    console.log(student.firstName + " " + student.lastName);
+    //sent an acknowledgment back to caller 
+    res.status(201).json('Post successful');
+  });
+  
+  app.use("/students", (req, res, next) => {
+    const students = [
+      { "id" : "1", "firstName" : "John" , "lastName" : "Dow" }, 
+      { "id" : "2", "firstName" : "Ann" , "lastName" : "Smith" }, 
+      { "id" : "3", "firstName" : "Joan" , "lastName" : "Doe" }
+   
+    ];
+    res.json(students);
+  });
+  
+  module.exports = app;
+  
